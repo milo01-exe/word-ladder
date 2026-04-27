75f75e4b0c1eb3f9e7fa64a7dfcea4458b745e55
milo-exe <marlowcharite@gmail.com>
2026-04-26 21:53:57 -0400

    Validate user input against word list

--- word_ladder.zig
+++ word_ladder.zig
@@ -1,6 +1,16 @@
 const std = @import("std");
 
+const InvalidWordError = error {
+    BadLength,
+    NotInWordList,
+    NotWordLadder,
+};
+
 pub fn main() !void {
+    var gpa: std.heap.DebugAllocator(.{}) = .init;
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
     var stdout_buffer: [1024]u8 = undefined;
     var stdin_buffer: [1024]u8 = undefined;
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
@@ -8,7 +18,24 @@ pub fn main() !void {
     const stdout = &stdout_writer.interface;
     const stdin = &stdin_reader.interface;
 
+    const seed: u64 = @intCast(std.time.milliTimestamp());
+    var pseudo_random_num_generator = std.Random.DefaultPrng.init(seed);
+    const random = pseudo_random_num_generator.random();
+    const start_index = random.intRangeLessThan(usize, 0, words.len);
+    const starting_word = words[start_index];
+
+    var ladder = std.ArrayList([4]u8).empty;
+    defer ladder.deinit(allocator);
+
+    try ladder.append(allocator, starting_word);
+
+    try stdout.print("Let's make a word ladder! Say `end` to exit.\n", .{});
+    try stdout.flush();
+
     while (true) {
+        const last_word = ladder.getLast();
+        try stdout.print("The ladder is {d} words long. The last word was {s}.\n", .{ladder.items.len, last_word});
+
         try stdout.print("> ", .{});
         try stdout.flush();
 
@@ -19,7 +46,18 @@ pub fn main() !void {
             break;
         }
 
-        try stdout.print("You said: {s}\n", .{input});
+        const word = validateWord(input, last_word) catch |e| {
+            const msg: []const u8 = switch (e) {
+                error.BadLength => "Hey, that's not four characters!",
+                error.NotInWordList => "I don't think that's a word…",
+                error.NotWordLadder => "That doesn't make a word ladder.",
+            };
+            
+            try stdout.print("{s}\n", .{msg});
+            continue;
+        };
+
+        try ladder.append(allocator, word);
     }
 
     try stdout.flush();
@@ -29,6 +67,35 @@ const words = blk: {
     @setEvalBranchQuota(1_000_000);
     break :blk parseWordList(@embedFile("words.txt"));
 };
+
+fn validateWord(input: []const u8, last_word: [4]u8) InvalidWordError![4]u8 {
+    if (input.len != 4) {
+        return error.BadLength;
+    }
+
+    const word = @as(*const [4]u8, @ptrCast(input.ptr)).*;
+
+    for (words) |wordlist_word| {
+        if (std.mem.eql(u8, &wordlist_word, &word)) {
+            break;
+        }
+    } else {
+        return error.NotInWordList;
+    }
+
+    var difference: u32 = 0;
+    for (word, last_word) |char_a, char_b| {
+        if (char_a != char_b) {
+            difference += 1;
+        }
+    }
+
+    if (difference != 1) {
+        return error.NotWordLadder;
+    }
+
+    return word;
+}
 
 fn parseWordList(comptime wordlist: []const u8) [countWordList(wordlist)][4]u8 {
     var list: [countWordList(wordlist)][4]u8 = undefined;
